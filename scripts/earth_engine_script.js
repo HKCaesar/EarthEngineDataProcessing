555064cf6ce1c3bdbc4773f17fe7cceeeb37b09c
mithrillion <aeroumbria@gmail.com>
2017-02-07 13:49:01 +1100

    updated training/prediction set output

--- scripts/earth_engine_script.js
+++ scripts/earth_engine_script.js
@@ -3,15 +3,6 @@ var area = ee.FeatureCollection(testArea);
 var coords = testPoint.coordinates().getInfo();  // use getInfo() to retrieve value
 Map.setCenter(coords[0], coords[1], 12);
 
-// // This function adds a time band to the image.
-// var createTimeBand = function(image) {
-//   // Scale milliseconds by a large constant to avoid very small slopes
-//   // in the linear regression output.
-//   return image.addBands(image.metadata('system:time_start').divide(1e18));
-//   // return image.addBands(ee.Date(image.get('system:time_start')));
-// };
-
-
 var collection = ee.ImageCollection('LANDSAT/LC8_SR')
     .filterDate('2015-03-01', '2016-02-01')
     .filterBounds(area);
@@ -42,8 +33,6 @@ Export.table.toDrive({
 var imgs = _collection.features.map(function(img){
   return ee.Image.load(img.id);
 });
-
-// Map.addLayer(imgs[5].clipToCollection(area), vizParams);
 
 imgs.forEach(function(img){
   Export.image.toDrive({
--- scripts/read_image_data_scaleable.py
+++ scripts/read_image_data_scaleable.py
@@ -5,7 +5,6 @@ import os
 import shelve
 import datetime
 import bisect
-from PIL import Image
 from multiprocessing import Pool
 
 
@@ -14,6 +13,8 @@ def create_folders(shelve_dir):
         os.mkdir(shelve_dir)
     if not os.path.exists(shelve_dir + 'old/'):
         os.mkdir(shelve_dir + 'old/')
+    if not os.path.exists(shelve_dir + 'train/'):
+        os.mkdir(shelve_dir + 'train/')
     if not os.path.exists(shelve_dir + 'old/maps'):
         os.mkdir(shelve_dir + 'old/maps')
     if not os.path.exists(shelve_dir + 'old/maps_interpolated'):
@@ -126,7 +127,7 @@ def interpolate(timestamp, maps, max_days_apart=None, shelve_dir=None):
     """
     Calculate the interpolated image at a given timestamp
     :param timestamp: timestamp for interpolation (as int)
-    :param dataset: a dict or shelf view for image data, assuming timestamps are strings
+    :param maps: a dict or shelf view for image data, assuming timestamps are strings
     :param max_days_apart: maximum days allowed between two interpolated value and a known value to be used as input
     in interpolation before the algorithm reports a missing value
     :return: the interpolated image array
@@ -206,30 +207,8 @@ def interpolate_images(timestamps, maps, max_days_apart=None, processes=1, shelv
         return imgs
 
 
-def convert_to_dataframe(key_image):
-    frame = pd.Panel(key_image[1]).to_frame()
-    return key_image[0], frame
-
-
-def make_set(images):
-    # times = list(images.keys())
-    # times.sort()
-    res = pd.concat([convert_to_dataframe(i)[1] for i in images.items()], axis=1, keys=images.keys())
-#     res = pd.concat(list(map(convert_to_dataframe, images.values())), axis=0)
-    return res.reset_index()
-
-
-def store_set(images, processes=1, shelve_dir=None):
-    try:
-        os.remove(shelve_dir + 'pixels.*')
-    except FileNotFoundError:
-        pass
-    pix = shelve.open(shelve_dir + 'pixels')
-    p = Pool(processes)
-    for ts, frame in p.imap(convert_to_dataframe, images.items()):
-        pix[ts] = frame.as_matrix()
-    p.close()
-    return pix
+def open_img(img_id, img_dir, res=None):
+    return np.memmap(img_dir + img_id, dtype='int16', mode='r', shape=res)
 
 
 def generate_coordinate_columns(x, y):
@@ -246,45 +225,44 @@ def extract_label_column(label_array):
     return label_set.sort_values(by=['x', 'variable'])['value'].as_matrix()
 
 
-class PixelRowReader(object):
+def extract_partial_set(index_range, imgs, img_dir, labels=None):
+    start, end = index_range
+    items = list(imgs.items())
+    items.sort()
+    df = pd.DataFrame()
+    for i in range(len(items)):
+        ts, res = items[i]
+        img = open_img(ts, img_dir, (res[0], res[1] * res[2]))
+        for band in range(3):
+            df[ts + '_' + str(band)] = img[band, start: end]
+    if labels is not None:
+        df['label'] = extract_label_column(labels)[start: end]
+    return df
 
-    def __init__(self, pixels, limits):
-        self.pixels = pixels
-        self.limits = limits
 
-    def __call__(self, ts):
-        return self.pixels[ts][self.limits[0]: self.limits[1], :]
-
-
-def combine_set(pixels, shelve_dir=None, res=None, step=250000, processes=1, labels=None):
-    times = list(pixels.keys())
-    times.sort()
-    try:
-        os.remove(shelve_dir + 'set.*')
-    except FileNotFoundError:
-        pass
-    pix_set = shelve.open(shelve_dir + 'set')
-    length = res[0] * res[1]
+def partial_set_iterator(step, imgs, img_dir, labels=None):
+    ts0, res0 = (next(iter(imgs.items())))
+    length = res0[1] * res0[2]
     if length < step:
         ranges = [(0, length)]
     else:
         stops = list(range(0, length, step))
         ranges = [(stops[i], stops[i+1]) for i in range(len(stops) - 1)] + [(stops[-1], length)]
+    for index_range in ranges:
+        yield index_range, extract_partial_set(index_range, imgs, img_dir, labels)
+
+
+def store_set(step, imgs, img_dir, shelve_dir, name='trains', labels=None):
+    try:
+        os.remove(shelve_dir + name + '.*')
+    except FileNotFoundError:
+        pass
+    trains = shelve.open(shelve_dir + name)
     n = 0
-    if labels is not None:
-        label_column = extract_label_column(labels)
-    else:
-        label_column = None
-    for limits in ranges:
-        p = Pool(processes)
-        pieces = p.map(PixelRowReader(pixels, limits), times)
-        p.close()
-        if label_column is not None:
-            pieces += [label_column[limits[0]: limits[1]].reshape(limits[1] - limits[0], 1)]
-        res = np.concatenate(pieces, axis=1)
-        pix_set[str(n)] = res
+    for train in partial_set_iterator(step, imgs, img_dir, labels):
+        trains[str(n)] = train
         n += 1
-    return pix_set
+    return trains
 
 
 def old_data_preprocess_workflow(image_dir, mask_dir, table_dir, shelve_root_dir, labels, new_table_dir=None,
@@ -295,7 +273,7 @@ def old_data_preprocess_workflow(image_dir, mask_dir, table_dir, shelve_root_dir
     :param mask_dir: directory of the mask files
     :param table_dir: path to the metadata table
     :param new_table_dir: path to the next year's metadata table (not used if timestamps are given)
-    :param shelve_root_dir: root directory for shelf files to be created
+    :param shelve_root_dir: root directory for shelf and memmap files to be created
     :param labels: class label map in array form
     :param max_days_apart: maximum days between interpolated value and an known value before reporting missing value
     :param processes: number of processes for multiprocessing
@@ -304,9 +282,9 @@ def old_data_preprocess_workflow(image_dir, mask_dir, table_dir, shelve_root_dir
     :return:
     """
     print("reading data...")
-    ds = read_image_data(image_dir, mask_dir, table_dir, shelve_root_dir + 'old/', processes)
+    maps = read_image_data(image_dir, mask_dir, table_dir, shelve_root_dir + 'old/', processes)
     print("reading new timestamps...")
-    res = ds[list(ds.keys())[0]].shape[1:]
+    # res = ds[list(ds.keys())[0]].shape[1:]
     new_table = pd.read_csv(new_table_dir)
     new_times = list(new_table['system:time_start'])
     if timestamps is None:
@@ -319,85 +297,218 @@ def old_data_preprocess_workflow(image_dir, mask_dir, table_dir, shelve_root_dir
         times_to_fit = timestamps
     times_to_fit.sort()
     print("interpolating images...")
-    imgs = interpolate_images(times_to_fit, ds, max_days_apart, processes, shelve_root_dir)
-    ds.close()
-    print("storing sets...")
-    pix = store_set(imgs, processes, shelve_root_dir + 'old/')
-    imgs.close()
-    print("combining sets...")
-    train = combine_set(pix, shelve_root_dir + 'old/', res, step, processes, labels)
-    return train
+    imgs = interpolate_images(times_to_fit, maps, max_days_apart, processes, shelve_root_dir + 'old/')
+    print("generating sets...")
+    trains = store_set(step, imgs, shelve_root_dir + 'old/maps_interpolated/', shelve_root_dir, 'trains', labels)
+    return trains
 
 
-# old
-def generate_interpolated_training_set(ds, ds_new=None, label_img_dir='labels/labels.png',
-                                       labels=None, max_days_apart=None):
-    # assuming dict keys are strings
-    keys = list(ds_new.keys())
-    times = [int(k) for k in keys]
-    times.sort()
-    times_to_fit = []
-    for t in times:
-        dt = datetime.datetime.fromtimestamp(t / 1000)
-        dt = dt.replace(year=dt.year - 1)
-        times_to_fit += [int(dt.timestamp() * 1000)]
-
-    fitted_images = interpolate_images(times_to_fit, ds, max_days_apart)
-    train = make_set(fitted_images)
-
-    if ds_new is not None:
-        new_no_cloud = interpolate_images(dataset=ds_new, timestamps=list(ds_new.keys()),
-                                          max_days_apart=max_days_apart)
-        to_predict = make_set(new_no_cloud)
-        to_predict.columns = [c for c in to_predict.columns]
-        to_predict = to_predict.rename(columns={('major', ''): 'x', ('minor', ''): 'y'})
-    else:
-        to_predict = None
-
-    if labels is None:
-        label_image = Image.open(label_img_dir)
-        labels = np.array(label_image)
-        pix_array = labels.reshape(labels.shape[0] * labels.shape[1], labels.shape[2])
-        label_colours = np.vstack({tuple(row) for row in pix_array})
-        label_map = {tuple(row): i for row, i in zip(label_colours, range(label_colours.shape[0]))}
-        label_converted = np.apply_along_axis(lambda x: label_map[tuple(x)], 2, labels)
-    else:
-        label_converted = labels
-
-    df = pd.DataFrame(label_converted)
-    df['x'] = df.index
-    label_set = pd.melt(df, id_vars='x')
-    train_labelled = pd.merge(train, label_set, left_on=['major', 'minor'], right_on=['x', 'variable'], copy=False)
-    train_labelled = train_labelled.drop(['x', 'variable'], axis=1).rename(
-        columns={('major', ''): 'x', ('minor', ''): 'y', 'value': 'label'})
-    return train_labelled, to_predict
+def new_data_preprocess_workflow(image_dir, mask_dir, table_dir, shelve_root_dir,
+                                 max_days_apart=60, processes=1, step=250000):
+    """
+    preprocess training data, interpolating it using the next year's available data timestamps
+    :param image_dir: directory of the image files
+    :param mask_dir: directory of the mask files
+    :param table_dir: path to the metadata table
+    :param shelve_root_dir: root directory for shelf and memmap files to be created
+    :param max_days_apart: maximum days between interpolated value and an known value before reporting missing value
+    :param processes: number of processes for multiprocessing
+    :param step: batch size for training set generation
+    :return:
+    """
+    print("reading data...")
+    maps = read_image_data(image_dir, mask_dir, table_dir, shelve_root_dir + 'new/', processes)
+    print("reading new timestamps...")
+    # res = ds[list(ds.keys())[0]].shape[1:]
+    new_table = pd.read_csv(table_dir)
+    times_to_fit = list(new_table['system:time_start'])
+    times_to_fit.sort()
+    print("interpolating images...")
+    imgs = interpolate_images(times_to_fit, maps, max_days_apart, processes, shelve_root_dir + 'new/')
+    print("generating sets...")
+    trains = store_set(step, imgs, shelve_root_dir + 'new/maps_interpolated/', shelve_root_dir, 'to_predict')
+    return trains
 
 
-def generate_interpolated_set_from_timestamps(ds, times, labels=None, on_self=False, max_days_apart=None):
-    # times need to be int already
-    times.sort()
-    times_to_fit = []
-    # TODO: separate timestamp lagging and fitting
-    if not on_self:
-        for t in times:
-            dt = datetime.datetime.fromtimestamp(t / 1000)
-            dt = dt.replace(year=dt.year - 1)
-            times_to_fit += [int(dt.timestamp() * 1000)]
-    else:
-        times_to_fit = times
+# def convert_to_dataframe(key_image):
+#     frame = pd.Panel(key_image[1]).to_frame()
+#     return key_image[0], frame
 
-    fitted_images = interpolate_images(times_to_fit, ds, max_days_apart)
-    train = make_set(fitted_images)
 
-    if labels is not None:
-        df = pd.DataFrame(labels)
-        df['x'] = df.index
-        label_set = pd.melt(df, id_vars='x')
-        train_labelled = pd.merge(train, label_set, left_on=['major', 'minor'], right_on=['x', 'variable'], copy=False)
-        train_labelled = train_labelled.drop(['x', 'variable'], axis=1).rename(
-            columns={('major', ''): 'x', ('minor', ''): 'y', 'value': 'label'})
-        return train_labelled
-    else:
-        train.columns = [c for c in train.columns]
-        train = train.rename(columns={('major', ''): 'x', ('minor', ''): 'y'})
-        return train
+# def make_set(images):
+#     # times = list(images.keys())
+#     # times.sort()
+#     res = pd.concat([convert_to_dataframe(i)[1] for i in images.items()], axis=1, keys=images.keys())
+# #     res = pd.concat(list(map(convert_to_dataframe, images.values())), axis=0)
+#     return res.reset_index()
+#
+#
+# def store_set(images, processes=1, shelve_dir=None):
+#     try:
+#         os.remove(shelve_dir + 'pixels.*')
+#     except FileNotFoundError:
+#         pass
+#     pix = shelve.open(shelve_dir + 'pixels')
+#     p = Pool(processes)
+#     for ts, frame in p.imap(convert_to_dataframe, images.items()):
+#         pix[ts] = frame.as_matrix()
+#     p.close()
+#     return pix
+
+
+
+# class PixelRowReader(object):
+#
+#     def __init__(self, pixels, limits):
+#         self.pixels = pixels
+#         self.limits = limits
+#
+#     def __call__(self, ts):
+#         return self.pixels[ts][self.limits[0]: self.limits[1], :]
+#
+#
+# def combine_set(pixels, shelve_dir=None, res=None, step=250000, processes=1, labels=None):
+#     times = list(pixels.keys())
+#     times.sort()
+#     try:
+#         os.remove(shelve_dir + 'set.*')
+#     except FileNotFoundError:
+#         pass
+#     pix_set = shelve.open(shelve_dir + 'set')
+#     length = res[0] * res[1]
+#     if length < step:
+#         ranges = [(0, length)]
+#     else:
+#         stops = list(range(0, length, step))
+#         ranges = [(stops[i], stops[i+1]) for i in range(len(stops) - 1)] + [(stops[-1], length)]
+#     n = 0
+#     if labels is not None:
+#         label_column = extract_label_column(labels)
+#     else:
+#         label_column = None
+#     for limits in ranges:
+#         p = Pool(processes)
+#         pieces = p.map(PixelRowReader(pixels, limits), times)
+#         p.close()
+#         if label_column is not None:
+#             pieces += [label_column[limits[0]: limits[1]].reshape(limits[1] - limits[0], 1)]
+#         res = np.concatenate(pieces, axis=1)
+#         pix_set[str(n)] = res
+#         n += 1
+#     return pix_set
+
+
+# def old_data_preprocess_workflow(image_dir, mask_dir, table_dir, shelve_root_dir, labels, new_table_dir=None,
+#                                  max_days_apart=60, processes=1, step=250000, timestamps=None):
+#     """
+#     preprocess training data, interpolating it using the next year's available data timestamps
+#     :param image_dir: directory of the image files
+#     :param mask_dir: directory of the mask files
+#     :param table_dir: path to the metadata table
+#     :param new_table_dir: path to the next year's metadata table (not used if timestamps are given)
+#     :param shelve_root_dir: root directory for shelf files to be created
+#     :param labels: class label map in array form
+#     :param max_days_apart: maximum days between interpolated value and an known value before reporting missing value
+#     :param processes: number of processes for multiprocessing
+#     :param step: batch size for training set generation
+#     :param timestamps: timestamps for interpolation (not necessary if new_table_dir is given)
+#     :return:
+#     """
+#     print("reading data...")
+#     ds = read_image_data(image_dir, mask_dir, table_dir, shelve_root_dir + 'old/', processes)
+#     print("reading new timestamps...")
+#     res = ds[list(ds.keys())[0]].shape[1:]
+#     new_table = pd.read_csv(new_table_dir)
+#     new_times = list(new_table['system:time_start'])
+#     if timestamps is None:
+#         times_to_fit = []
+#         for t in new_times:
+#             dt = datetime.datetime.fromtimestamp(t / 1000)
+#             dt = dt.replace(year=dt.year - 1)
+#             times_to_fit += [int(dt.timestamp() * 1000)]
+#     else:
+#         times_to_fit = timestamps
+#     times_to_fit.sort()
+#     print("interpolating images...")
+#     imgs = interpolate_images(times_to_fit, ds, max_days_apart, processes, shelve_root_dir)
+#     ds.close()
+#     print("storing sets...")
+#     pix = store_set(imgs, processes, shelve_root_dir + 'old/')
+#     imgs.close()
+#     print("combining sets...")
+#     train = combine_set(pix, shelve_root_dir + 'old/', res, step, processes, labels)
+#     return train
+
+
+# # old
+# def generate_interpolated_training_set(ds, ds_new=None, label_img_dir='labels/labels.png',
+#                                        labels=None, max_days_apart=None):
+#     # assuming dict keys are strings
+#     keys = list(ds_new.keys())
+#     times = [int(k) for k in keys]
+#     times.sort()
+#     times_to_fit = []
+#     for t in times:
+#         dt = datetime.datetime.fromtimestamp(t / 1000)
+#         dt = dt.replace(year=dt.year - 1)
+#         times_to_fit += [int(dt.timestamp() * 1000)]
+#
+#     fitted_images = interpolate_images(times_to_fit, ds, max_days_apart)
+#     train = make_set(fitted_images)
+#
+#     if ds_new is not None:
+#         new_no_cloud = interpolate_images(dataset=ds_new, timestamps=list(ds_new.keys()),
+#                                           max_days_apart=max_days_apart)
+#         to_predict = make_set(new_no_cloud)
+#         to_predict.columns = [c for c in to_predict.columns]
+#         to_predict = to_predict.rename(columns={('major', ''): 'x', ('minor', ''): 'y'})
+#     else:
+#         to_predict = None
+#
+#     if labels is None:
+#         label_image = Image.open(label_img_dir)
+#         labels = np.array(label_image)
+#         pix_array = labels.reshape(labels.shape[0] * labels.shape[1], labels.shape[2])
+#         label_colours = np.vstack({tuple(row) for row in pix_array})
+#         label_map = {tuple(row): i for row, i in zip(label_colours, range(label_colours.shape[0]))}
+#         label_converted = np.apply_along_axis(lambda x: label_map[tuple(x)], 2, labels)
+#     else:
+#         label_converted = labels
+#
+#     df = pd.DataFrame(label_converted)
+#     df['x'] = df.index
+#     label_set = pd.melt(df, id_vars='x')
+#     train_labelled = pd.merge(train, label_set, left_on=['major', 'minor'], right_on=['x', 'variable'], copy=False)
+#     train_labelled = train_labelled.drop(['x', 'variable'], axis=1).rename(
+#         columns={('major', ''): 'x', ('minor', ''): 'y', 'value': 'label'})
+#     return train_labelled, to_predict
+#
+#
+# def generate_interpolated_set_from_timestamps(ds, times, labels=None, on_self=False, max_days_apart=None):
+#     # times need to be int already
+#     times.sort()
+#     times_to_fit = []
+#     # TODO: separate timestamp lagging and fitting
+#     if not on_self:
+#         for t in times:
+#             dt = datetime.datetime.fromtimestamp(t / 1000)
+#             dt = dt.replace(year=dt.year - 1)
+#             times_to_fit += [int(dt.timestamp() * 1000)]
+#     else:
+#         times_to_fit = times
+#
+#     fitted_images = interpolate_images(times_to_fit, ds, max_days_apart)
+#     train = make_set(fitted_images)
+#
+#     if labels is not None:
+#         df = pd.DataFrame(labels)
+#         df['x'] = df.index
+#         label_set = pd.melt(df, id_vars='x')
+#         train_labelled = pd.merge(train, label_set, left_on=['major', 'minor'], right_on=['x', 'variable'], copy=False)
+#         train_labelled = train_labelled.drop(['x', 'variable'], axis=1).rename(
+#             columns={('major', ''): 'x', ('minor', ''): 'y', 'value': 'label'})
+#         return train_labelled
+#     else:
+#         train.columns = [c for c in train.columns]
+#         train = train.rename(columns={('major', ''): 'x', ('minor', ''): 'y'})
+#         return train
